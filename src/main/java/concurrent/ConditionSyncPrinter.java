11e5e03ec274f8d4a40401cce6bff1dbd4e1c71b
qilinxiaoxiang <wq5513031@126.com>
2019-08-06 23:46:12 +0800

    feature: change the name

--- src/main/java/concurrent/ConditionSyncPrinter.java
+++ src/main/java/concurrent/ConditionSyncPrinter.java
@@ -1,16 +1,15 @@
 package concurrent;
 
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
-public class SyncPrinter implements Runnable{
-    public static final int PRINT_COUNT = 10;
+public class ConditionSyncPrinter implements Runnable{
+    private static final int PRINT_COUNT = 10;
     private final CloseableReentrantLock lock;
     private final Condition thisCondition;
     private final Condition nextCondition;
     private final char printChar;
 
-    public SyncPrinter(CloseableReentrantLock lock, Condition thisCondition, Condition nextCondition, char printChar) {
+    public ConditionSyncPrinter(CloseableReentrantLock lock, Condition thisCondition, Condition nextCondition, char printChar) {
         this.lock = lock;
         this.thisCondition = thisCondition;
         this.nextCondition = nextCondition;
--- src/main/java/concurrent/Main.java
+++ src/main/java/concurrent/Main.java
@@ -5,13 +5,13 @@ import java.util.concurrent.locks.Condition;
 public class Main {
     public static void main(String[] args) {
         CloseableReentrantLock lock = new CloseableReentrantLock();
-        Condition printA = lock.newCondition();
-        Condition printB = lock.newCondition();
-        Condition printC = lock.newCondition();
-        Thread printerA = new Thread(new SyncPrinter(lock, printA, printB, 'A'));
-        Thread printerB = new Thread(new SyncPrinter(lock, printB, printC, 'B'));
-        Thread printerC = new Thread(new SyncPrinter(lock, printC, printA, 'C'));
-        Thread signaler = new Thread(new Signaler(lock, printA));
+        Condition conditionA = lock.newCondition();
+        Condition conditionB = lock.newCondition();
+        Condition conditionC = lock.newCondition();
+        Thread printerA = new Thread(new ConditionSyncPrinter(lock, conditionA, conditionB, 'A'));
+        Thread printerB = new Thread(new ConditionSyncPrinter(lock, conditionB, conditionC, 'B'));
+        Thread printerC = new Thread(new ConditionSyncPrinter(lock, conditionC, conditionA, 'C'));
+        Thread signaler = new Thread(new Signaler(lock, conditionA));
         printerA.start();
         printerB.start();
         printerC.start();
@@ -19,13 +19,13 @@ public class Main {
         signaler.start();
         // signal printerA in main thread is not reliable, if sleep less than 100 millis, the signal method may fail
         // and the printerA will await forever
-//        try {
-//            Thread.sleep(100);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        try(CloseableReentrantLock closeableLock = lock.open()) {
-//            printA.signalAll();
-//        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try(CloseableReentrantLock ignored = lock.open()) {
+            conditionA.signalAll();
+        }
     }
 }
